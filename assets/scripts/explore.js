bd7e36105775aa5f4a3a86a64220e8803065ee42
Grant Cheng <26587113+CatFish47@users.noreply.github.com>
2023-04-29 16:44:38 -0700

    Finish part 1 of explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,62 @@
 // explore.js
+let synth, voiceObj;
+let faceImg, textInput, voiceSelect, playButton;
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  synth = window.speechSynthesis;
+  voiceObj = {};
+
+  faceImg = document.querySelector('img');
+  textInput = document.getElementById('text-to-speak');
+  voiceSelect = document.getElementById('voice-select');
+  playButton = document.querySelector('button');
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  playButton.addEventListener('click', () => {
+    speak();
+  });
+}
+
+function populateVoiceList() {
+  const voices = synth.getVoices();
+
+  for (let voice of voices) {
+    const { name, lang } = voice;
+
+    voiceObj[name] = voice;
+
+    const option = document.createElement("option");
+    option.textContent = `${name} (${lang})`;
+
+    if (voice.default) {
+      option.textContent += " - DEFAULT";
+    }
+
+    option.setAttribute('data-lang', lang);
+    option.setAttribute('data-name', name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+function speak() {
+  const textSpeak = new SpeechSynthesisUtterance(textInput.value);
+  const voice = voiceObj[voiceSelect.selectedOptions[0].getAttribute('data-name')];
+  textSpeak.voice = voice;
+  synth.speak(textSpeak);
+
+  textSpeak.addEventListener('start', () => {
+    faceImg.src = "assets/images/smiling-open.png";
+    faceImg.alt = "Talking face";
+  });
+
+  textSpeak.addEventListener('end', () => {
+    faceImg.src = "assets/images/smiling.png";
+    faceImg.alt = "Smiling face";
+  });
 }
